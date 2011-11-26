260c78f9345e35c5ab3c94e2de15e2d7d22f7be2
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-11-26 22:59:56 +0100

    Implemented encoding and decoding.
    Implemented encoding and decoding commands using
    NameTransformer. In order to test it just use curl:
    
    curl -d 'q=foo!!:' http://localhost:8080/enc
    foo$bang$bang$colon

--- src/main/scala/gt4s/Start.scala
+++ src/main/scala/gt4s/Start.scala
@@ -7,7 +7,19 @@ import util.Properties
 
 object Hello extends cycle.Plan with cycle.ThreadPool with ServerErrorResponse {
   def intent = {
-		 case GET(_) => Ok ~> ResponseString("Welcome to gt4s")
+    case POST(Path("/enc") & Params(params)) =>
+      params("q") match {
+        case List(q) => Ok ~> ResponseString(scala.reflect.NameTransformer.encode(q))
+        case _ =>       BadRequest ~> ResponseString("Post data to be encoded in q parameter.")
+      }
+    case POST(Path("/dec") & Params(params)) =>
+      params("q") match {
+        case List(q) => Ok ~> ResponseString(scala.reflect.NameTransformer.decode(q))
+        case _ =>       BadRequest ~> ResponseString("Post data to be encoded in q parameter.")
+      }
+    case GET(Path("/enc") | Path("/dec")) =>
+      MethodNotAllowed ~> ResponseString("Use POST HTTP method.")
+    case GET(_) => Ok ~> ResponseString("Welcome to gt4s. POST data to /enc or /dec using `q` parameter.")
   }
 }
 
