5a6291cab55b7c4a4af01061fa2c2a690b2bb036
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-11-26 21:39:34 +0100

    Added debugging line.

--- src/main/scala/gt4s/Start.scala
+++ src/main/scala/gt4s/Start.scala
@@ -13,5 +13,6 @@ object Hello extends cycle.Plan with cycle.ThreadPool with ServerErrorResponse {
 
 object Start extends App {
   val port = Properties.envOrElse("PORT", "80").toInt
+  println("Starting on port:"+port)
   unfiltered.netty.Http(port).plan(Hello).run()
 }
