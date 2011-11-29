6a50e7b7785a906587b860fb5e686f4e79691c52
Fredrik Ekholdt <fredrik.ekholdt@typesafe.com>
2011-11-29 10:27:01 +0100

    fixed non unchecked warnings when using type pattern List

--- src/main/scala/gt4s/Start.scala
+++ src/main/scala/gt4s/Start.scala
@@ -14,12 +14,12 @@ object Hello extends cycle.Plan with cycle.ThreadPool with ServerErrorResponse {
   def intent = {
     case POST(Path("/enc") & Params(params)) =>
       params("q") match {
-        case List(q) => Ok ~> ResponseString(scala.reflect.NameTransformer.encode(q))
+        case Seq(q) => Ok ~> ResponseString(scala.reflect.NameTransformer.encode(q))
         case _ =>       BadRequest ~> ResponseString("Post data to be encoded in q parameter.")
       }
     case POST(Path("/dec") & Params(params)) =>
       params("q") match {
-        case List(q) => Ok ~> ResponseString(scala.reflect.NameTransformer.decode(q))
+        case Seq(q) => Ok ~> ResponseString(scala.reflect.NameTransformer.decode(q))
         case _ =>       BadRequest ~> ResponseString("Post data to be encoded in q parameter.")
       }
     case GET(Path("/enc") | Path("/dec")) =>
